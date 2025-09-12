faec7042b4cfa9814f60073952a95c1bafb75f59
Minh07 <minhhk@gmail.com>
2025-09-12 17:14:22 +0700

    add set-password and fix message code

---
+++ BE_ Small Shop Management System/DTOs/SetPasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BE__Small_Shop_Management_System.DTOs
+{
+    public class SetPasswordRequest
+    {
+        public string? CurrentPassword { get; set; }  // có thể null nếu là reset từ admin
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
---
+++ BE_ Small Shop Management System/Helper/ApiResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace BE__Small_Shop_Management_System.Helper
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }  
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public IEnumerable<object>? Errors { get; set; }
+
+        // Success
+        public static ApiResponse<T> SuccessResponse(T data, string message = "", int statusCode = 200)
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                Data = data,
+                Message = message,
+                StatusCode = statusCode
+            };
+        }
+
+        // Error
+        public static ApiResponse<T> ErrorResponse(string message, IEnumerable<object>? errors = null, int statusCode = 400)
+        {
+            return new ApiResponse<T>
+            {
+                Success = false,
+                Message = message,
+                Errors = errors,
+                StatusCode = statusCode
+            };
+        }
+    }
+}
